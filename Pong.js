1912cda9570855a1fe179d2226422f5e4b44967b
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2018-10-23 15:16:12 -0400

    Readme mejorado. Codigos Agregados.

---
+++ Pong.js
@@ -0,0 +1,190 @@
+//variables globales
+var bucle;
+var velocidad = 5;
+var canvas = document.getElementById('canvas');
+var AreaH = canvas.height;
+var AreaW = canvas.width;
+var ctx = canvas.getContext('2d');
+var puntosj1 = 0;
+var puntosj2 = 0;
+var tamanoP = 75;
+var superficie = AreaH-tamanoP;
+//
+function Pausa() {
+	alert("Pausa");
+}
+
+function gana() {
+	if (puntosj1 > 19) {
+		alert("Gano el jugador "+ju1);
+		document.location.reload();
+	}
+	if (puntosj2 > 19) {
+		alert("Gano el jugador "+ju2);
+		document.location.reload();
+	}
+}
+
+ju1 = prompt("Escriba el nombre del jugador 1");
+ju2 = prompt("Escriba el nombre del jugador 2");
+//clases
+class Base {
+	choque(obj) {
+		if (this.fondo < obj.y || this.y > obj.fondo || this.derecha < obj.x || this.x > obj.derecha) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
+class Puntos {
+	constructor(x) {
+		this.x = x;
+		this.y = 25;
+		this.punto = 0;
+	}
+	dibujar() {
+		ctx.font = "25px Arial";
+		ctx.fillText(this.punto.toString(), this.x, this.y);
+	}
+}
+class Bola extends Base {
+	constructor() {
+		super();
+		this.t = 25;
+		this.x = Math.floor(Math.random() * (AreaW - this.t));
+		this.y = Math.floor(Math.random() * (AreaH - this.t));
+		this.xdir = velocidad;
+		this.ydir = velocidad;
+		this.p1 = new Puntos(25);
+		this.p2 = new Puntos(575);
+	}
+	choqueV() {
+		if (this.y <= 0 || this.y >= (AreaH -this.t)) {
+			this.ydir = -this.ydir;
+		}
+	}
+	choqueH() {
+		if (this.x <= 0) {
+			this.xdir = -this.xdir;
+			puntosj2++;
+			this.p2.punto = puntosj2;
+		}
+		if (this.x >= (AreaW - this.t)) {
+			this.xdir = -this.xdir; 
+			puntosj1++;
+			this.p1.punto = puntosj1;
+		}
+	}
+	mover() {
+		this.x+=this.xdir;
+		this.y+=this.ydir;
+		this.fondo = this.y+this.t;
+		this.derecha = this.x+this.t;
+		this.choqueV(); 
+		this.choqueH();
+	}
+	dibujar() {
+		ctx.fillRect(this.x, this.y, this.t, this.t);
+		this.p1.dibujar();
+		this.p2.dibujar();
+	}
+}
+class Paleta extends Base {
+	constructor(x) {
+		super();
+		this.x = x;
+		this.w = 25;
+		this.h = tamanoP;
+		this.y = Math.floor(Math.random() * superficie);
+		this.dir = 0;
+	}
+	dibujar() {
+		ctx.fillRect(this.x,this.y,this.w,this.h);
+	}
+	mover() {
+		this.y+=this.dir;
+		this.derecha = this.w+this.x;
+		this.fondo = this.h+this.y;
+		if (this.y <= 0) {
+			this.y = 0;
+			this.dir = 0;
+		}
+		if (this.y >= superficie) {
+			this.y = superficie;
+			this.dir = 0;
+		}
+	}
+}
+class jugadores {
+	constructor() {
+
+	}
+	dibujar() {
+		ctx.fillStyle = 'black';
+		ctx.font = "15px Arial";
+		ctx.fillText("El jugador 1 es: " +ju1, 10, 290);
+		ctx.fillStyle = 'black';
+		ctx.font = "15px Arial";
+		ctx.fillText("El jugador 2 es: " +ju2, 458, 290);
+	}
+}
+//Objetos
+var bola = new Bola();
+var jugador1 = new Paleta(30);
+var jugador2 = new Paleta(545);
+var ju = new jugadores();
+//Funciones de control
+function moverP(event) {
+	var tecla = event.keyCode;
+	if (tecla == 38) {
+		jugador2.dir = -velocidad;
+	}
+	if (tecla == 40) {
+		jugador2.dir = velocidad;
+	}
+	if (tecla == 87) {
+		jugador1.dir = -velocidad;
+	}
+	if (tecla == 83) {
+		jugador1.dir = velocidad;
+	}
+}
+function pararP(event) {
+	var tecla = event.keyCode;
+	if (tecla == 38 || tecla == 40) {
+		jugador2.dir = 0;
+	}
+	if (tecla == 87 || tecla == 83) {
+		jugador1.dir = 0;
+	}
+}
+function choque() {
+	if (bola.choque(jugador1) || bola.choque(jugador2)) {
+		bola.xdir = -bola.xdir;
+	}
+}
+//funciones globales
+function dibujar() {
+	ctx.clearRect(0,0,AreaW, AreaH);
+	bola.dibujar();
+	jugador1.dibujar();
+	jugador2.dibujar();
+	ju.dibujar();
+}
+
+function frame() {
+	bola.mover();
+	jugador1.mover();
+	jugador2.mover();
+	dibujar();
+	choque();
+	gana();
+	bucle = requestAnimationFrame(frame);
+}
+
+function iniciar() {
+	var modal = document.getElementById('modal');
+	modal.style.display = "none";
+	frame();
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Pong
+> Es una aplicacion desarrollada en Javascript,Html y CSS; Es un juego basado en Pong de dos jugadores
+- Es un juego de practica que desarrolle
+- Tiene unos errores pequeños que se pueden mejorar
+
+## Instalación
+
+- Funciona en Linux y Window
+
+> Abrir el html en un navegador
